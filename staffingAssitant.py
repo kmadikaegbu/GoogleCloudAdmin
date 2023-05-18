4a3253f4c5d1bf838bc86dd25fada33c4eb8001d
kmadikaegbu <kmadikaegbu@gmail.com>
2023-05-17 17:27:27 -0700

    hackathon

---
+++ staffingAssitant.py
@@ -0,0 +1,16 @@
+print ("Hello World!")
+print ("Hello Again")
+
+# create a sql table with the following columns
+# 1. ldap: string
+# 2. role: string
+# 3. Practice: string
+# 4. Skills: string (comma separated)
+# 5. Assignment1Startdate: date
+# 6. Assignment1Enddate: date
+# 7. Assignment2Startdate: date
+# 8. Assignment2Enddate: date
+# 9. AssignmentHours: integer
+
+
+
